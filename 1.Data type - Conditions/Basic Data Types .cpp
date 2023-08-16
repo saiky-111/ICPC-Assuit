0d79b090b6927bf7440021fac0411a1a6e3e7e49
Saikat Dhibar <97330742+saiky-111@users.noreply.github.com>
2023-08-16 19:15:04 +0530

    Add files via upload

---
+++ 1.Data type - Conditions/Basic Data Types .cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+ ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	    int x;
+	     long long y;char z; float k; double j;
+	    cin>>x>>y>>z>>k>>j;
+	    cout<<x<<endl;
+	    cout<<y<<endl;
+	    cout<<z<<endl;
+	    cout<<k<<endl;
+	    cout<<j<<endl;
+
+	return 0;
+}

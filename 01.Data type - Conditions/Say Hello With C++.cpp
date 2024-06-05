2f7122e8a1ffae5a835e44729d81f482b40c179e
Saikat Dhibar <saikat111dhibar@gmail.com>
2024-06-05 22:41:27 +0530

    initial commit

---
+++ 01.Data type - Conditions/Say Hello With C++.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define long long ll
+using namespace std;
+
+int main() {
+ ios::sync_with_stdio(false);
+    cin.tie(NULL);
+	string s;
+	cin>>s;
+	cout<<"Hello, "<<s<<endl;
+	   
+	return 0;
+}
